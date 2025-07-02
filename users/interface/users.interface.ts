d38e4c1ff5d346b5581551e969de0a312e050072
MelinaNorton <melinan@clemson.edu>
2025-07-02 08:08:43 -0400

    detailed interfaces & schemas

---
+++ users/interface/users.interface.ts
@@ -0,0 +1,8 @@
+export interface Users {
+    firstname : string,
+    lastname : string,
+    username : string,
+    password : string,
+    items : Array<string>,
+    email : string
+}
---
+++ users/schema/users.schema.ts
@@ -0,0 +1,10 @@
+import { Schema } from 'mongoose'
+
+export const userSchema = new Schema({
+    firstname: {type:String, required:true},
+    lastname: {type:String, required:true},
+    username: {type:String, required:true},
+    password: {type:String, required:true},
+    email: {type:String, required:true},
+    items: {type:Array<String>, required:true},
+})
