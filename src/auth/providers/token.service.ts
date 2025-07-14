d076b67558df7ba61ff464a5fa4b22ff50bc1f89
MelinaNorton <linamelina0707@gmail.com>
2025-07-14 03:38:46 -0400

    Pushing lats commit before file structure changes

--- src/auth/providers/token.service.ts
+++ src/auth/providers/token.service.ts
@@ -4,12 +4,17 @@ import { JwtService } from "@nestjs/jwt"
 import { User } from "src/users/interface/user.interface"
 import { Res } from "@nestjs/common"
 import { Response } from 'express';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class TokenService {
-    private readonly tokenname;
-    private readonly secret;
-    private readonly expiry;
+    private readonly refreshtokenname;
+    private readonly refreshsecret;
+    private readonly refreshexpiry;
+    private readonly accesstokenname;
+    private readonly accesssecret;
+    private readonly accessexpiry;
+
 //constructor defining our instances of JwtService & ConfigService for later use
     constructor(
         private jwtService: JwtService,
@@ -18,24 +23,35 @@ export class TokenService {
 
 //pull in config values for reference (note configService needs to be called within tge constructior, since the code below
 //will) run first before configService is initialized & throw an error
-        this.tokenname = 'token'
-        this.secret = this.configService.get("JWT_SECRET")
-        this.expiry = this.configService.get("JWT_EXPIRES")
+        this.refreshtokenname = 'refreshtoken'
+        this.refreshsecret = this.configService.get("JWT_REFRESH_SECRET")
+        this.refreshexpiry = this.configService.get("JWT_REFRESH_EXPIRES")
+        this.accesstokenname = 'accesstoken'
+        this.accesssecret = this.configService.get("JWT_ACCESS_SECRET")
+        this.accessexpiry = this.configService.get("JWT_ACCESS_EXPIRES")
     }
 
 //sign function for craeting our authenticated user with the token needed to access certain endpoints via jwtService's
 //sign() function & the .env values pulled using configService
     async createToken(user:User):Promise<string>{
-        const token = await this.jwtService.sign({ id: user._id, username: user.username }, {
-            secret: this.secret,
-            expiresIn: this.expiry,
+        const jti = randomUUID()
+        const token = await this.jwtService.sign({ sub: user._id, username: user.username, type:"refresh" , jti}, {
+            secret: this.refreshsecret,
+            expiresIn: this.refreshexpiry,
       })
       return token;
     }
 
+    async createAccessToken(user:User):Promise<string>{
+        const token = await this.jwtService.sign({ sub:user._id, username: user.username, type:"access"},{
+            secret: this.accesssecret,
+            expiresIn: this.accessexpiry
+        })
+        return token;
+    }
 //actual logic for attaching the above-created token to the Response object with our desired parameters
     async attatchToken(token:string, res: Response):Promise<boolean>{
-        res.cookie(this.tokenname, token, {
+        res.cookie(this.refreshtokenname, token, {
             httpOnly: true,
             secure: true,
             sameSite: 'none',
@@ -48,7 +64,7 @@ export class TokenService {
 //detach the authenticated token on logout, again using the Response object
     async detatchToken(@Res({ passthrough: true }) res: Response):Promise<boolean>{
         console.log("RAN!")
-        res.clearCookie(this.tokenname, {
+        res.clearCookie(this.refreshtokenname, {
             httpOnly: true,
             secure: true,
             sameSite: 'none',
@@ -56,4 +72,24 @@ export class TokenService {
         })
         return true
     }
+
+    /*async validateRefreshToken(token:string):Promise<string>{
+        const decodedToken = await this.jwtService.verifyAsync<{sub:string, username:string, type:string, jti:string}>(
+        token,
+        {secret: this.refreshsecret}
+        )
+        if(decodedToken.type != "refresh"){
+            throw new UnauthorizedError("Attempted to use an access token as a refresh token")
+        }
+        //find the jti in mongodb
+        return decodedToken
+    }*/
+
+    /*async validateAccessToken(token:string):Promise<strinng>{
+        const decodedToken = await this.jwtService.verifyAsync<{sub:string, username:string, type:string, jti:string}>(
+        token,
+        {secret: this.refreshsecret}
+        )
+        return decodedToken
+    }*/
 }
