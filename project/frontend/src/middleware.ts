7882842d69f5eaad12cff57284fe2e9ded355da0
MelinaNorton <linamelina0707@gmail.com>
2025-07-24 10:21:54 -0400

    configured route protecting middleware

---
+++ project/frontend/src/middleware.ts
@@ -0,0 +1,28 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest) {
+
+    const token = request.cookies.get('refreshtoken')?.value;
+    console.log('refreshtoken', token)
+    if (!token) {
+        if (!['/login', '/signup'].includes(request.nextUrl.pathname)) {
+            return NextResponse.redirect(new URL('/login', request.url));
+        }
+        return NextResponse.next();
+    }
+    else if (token) {
+        if (request.nextUrl.pathname == "/login") {
+            return NextResponse.redirect(new URL('/home', request.url));
+        }
+        else {
+            return NextResponse.next();
+        }
+    }
+}
+
+export const config = {
+    matcher: [
+        '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+    ],
+}
